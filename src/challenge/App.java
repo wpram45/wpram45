95bb88afd7a002fc564130093000d1aeadc13c37
serif geyik <wpram734@gmail.com>
2022-06-15 20:43:21 +0300

    commit1

---
+++ src/challenge/App.java
@@ -0,0 +1,19 @@
+package challenge;
+
+public class App {
+	
+	public static int[] resolve(int[] arr) {
+		
+		arr[0]=(arr[0]== 1) ?  0 :1;
+		arr[1]=(arr[0]== 1) ?  0 :1;
+		
+		
+		return arr;
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("test");
+		
+	}
+}
